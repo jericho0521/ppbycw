3fe7fe8df639531f9bb2e436844467f79b2b6f79
Jericho <jericho0521@gmail.com>
2024-09-22 23:55:44 +0800

    reservation caption

--- src/Home.jsx
+++ src/Home.jsx
@@ -33,7 +33,7 @@ function Home() {
             <h4 className="montserrat-bold">RESERVATION</h4>
             </a>
             <br></br>
-            <h6>-- ONLY FOR RACING SIMMULATOR & PLAYSTATION 5--</h6>
+            <h6>*APPLICABLE ONLY FOR PLAYSTATION 5 & RACING SIMULATOR </h6>
           </div>
 
       {/* SERVICES AND RIGS SECTION */}
