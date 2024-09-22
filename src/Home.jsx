b6419da7680bd6388bef554d458920019060188b
Jericho <jericho0521@gmail.com>
2024-09-22 23:19:23 +0800

    reservation button

--- src/Home.jsx
+++ src/Home.jsx
@@ -30,8 +30,10 @@ function Home() {
           <img src={logo} className="image-item" alt="Project Play Logo" />
           </div>
             <a href="https://wa.me/601116281524" target="_blank" className="button">
-            <h4 className="montserrat-bold">CONTACT US</h4>
+            <h4 className="montserrat-bold">RESERVATION</h4>
             </a>
+            <br></br>
+            <h6>-- ONLY FOR RACING SIMMULATOR & PLAYSTATION 5--</h6>
           </div>
 
       {/* SERVICES AND RIGS SECTION */}
