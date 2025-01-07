f955c86b1741134a27ef0edf5446939c0876465d
Jericho <jericho0521@gmail.com>
2025-01-07 19:28:24 +0800

    inner text and font size update

--- src/Pages/Events.jsx
+++ src/Pages/Events.jsx
@@ -23,10 +23,13 @@ function Events() {
             <div className="event-details">
                 <div className="row-bar4">
                     <h1>EVENTS WITH PROJECT PLAY</h1>
+                    <div class="innertext">
                     <p>If you're looking for the perfect venue to host your next event, we’ve got you covered. Find us at Project Play!</p>
+                    </div>
                     <a href="https://wa.me/601116281524" target="_blank" rel="noopener noreferrer" className="button-event">
                         CONTACT US
                     </a>
+                    
                 </div>
             </div>
 
@@ -34,14 +37,16 @@ function Events() {
                 <div className="past-events">
                     <h1>PAST EVENTS</h1>
                     <h3>Monash Cup 2024</h3>
+                    <div class="innertext">
                     <p>
-                        Highlights: Monash University hosted their Monash Cup 2024 at Project Play. Monash Cup was held for 3 days and had teams competing in 3 different games:
+                        Highlights: Monash University hosted their Monash Cup 2024 at Project Play. Monash Cup was held for 3 days and had teams competing in different games including:
                         <ul>
-                            <li>Counter Strike 2</li>
-                            <li>Valorant</li>
-                            <li>League of Legends</li>
+                            <br />Counter Strike 2
+                            <br />Valorant
+                            <br />League of Legends
                         </ul>
                     </p>
+                    </div>
 
                     <ImageRow images={images} />
                 </div>
