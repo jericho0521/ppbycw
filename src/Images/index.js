e85d6071050abf35ac0d0a7ef6fe98ac8dc42b9d
Jericho <73098876+jericho0521@users.noreply.github.com>
2025-06-22 12:27:24 +0800

    webp to png

--- src/Images/index.js
+++ src/Images/index.js
@@ -1,21 +1,21 @@
-import logo from './Project Play_Logo.webp';
-import image1 from './rs_ppbycw.webp';
-import image2 from './pc_ppbycw.webp';
-import image3 from './ps5new.webp';
-import com1 from './community1.webp';
-import com2 from './community2.webp';
-import com3 from './community3.webp';
-import com4 from './community4.webp';
-import com5 from './community5.webp';
-import com6 from './community6.webp';
-import store from './store.webp';
-import mc1 from './mc1.webp';
-import mc2 from './monash2.webp';
-import mc3 from './monash3.webp';
-import mc4 from './monash4.webp';
-import mc5 from './monash5.webp';
-import mc6 from './monash6.webp';
-import rnr from './rnr.webp';
+import logo from './Project Play_Logo.png';
+import image1 from './rs_ppbycw.png';
+import image2 from './pc_ppbycw.png';
+import image3 from './ps5new.png';
+import com1 from './community1.png';
+import com2 from './community2.png';
+import com3 from './community3.png';
+import com4 from './community4.png';
+import com5 from './community5.png';
+import com6 from './community6.png';
+import store from './store.png';
+import mc1 from './mc1.png';
+import mc2 from './monash2.png';
+import mc3 from './monash3.png';
+import mc4 from './monash4.png';
+import mc5 from './monash5.png';
+import mc6 from './monash6.png';
+import rnr from './rnr.png';
 
 export {
   logo,
